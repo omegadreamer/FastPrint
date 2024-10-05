34a5d0d8e0d0f44f8b884ddad007c2abd3d8c34a
omegadreamer <lolromik04@gmail.com>
2024-10-05 23:50:56 +0300

    some attempts

--- main/static/practice.js
+++ main/static/practice.js
@@ -1,16 +1,24 @@
-// Selectors
-const Words = document.getElementById("Words");
-
-Words.textContent = "С другой стороны постоянный количественный рост и сфера нашей активности представляет собой интересный эксперимент проверки направлений прогрессивного развития. Идейные соображения высшего порядка, а также сложившаяся структура организации позволяет оценить значение позиций, занимаемых участниками в отношении поставленных задач. Товарищи! укрепление и развитие структуры обеспечивает широкому кругу (специалистов) участие в формировании форм развития."
+let Words = document.getElementById("Words");
+const Text = "С другой стороны постоянный количественный рост и сфера нашей активности представляет собой интересный эксперимент проверки направлений прогрессивного развития.";
 
 // Variables
 let startTime;
-let words = Words.textContent;
 let inputbox = document.getElementById("InputBox");
 
+Words.textContent = Text
+
 document.getElementById("InputBox").addEventListener('input', function(event) {
-  console.log(words)
-  if(inputbox.value != words){
-    console.log(inputbox.value)
+  console.log(document.getElementById("Words"))
+  if(Text.slice(0, String(inputbox.value).length) == inputbox.value){
+    console.log("Праильна")
+    console.log("текст - " + Text.slice(0, String(inputbox.value).length))
+    console.log("ввод - " + inputbox.value)
   }
+
+  else{
+    console.log("Непраильна")
+    console.log("текст - " + Text.slice(0, String(inputbox.value).length))
+    console.log("ввод - " + inputbox.value)
+  }
+  
 });
