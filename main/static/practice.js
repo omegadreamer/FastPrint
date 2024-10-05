246f6f6aae485b8a06832573d1d5c804433359db
omegadreamer <lolromik04@gmail.com>
2024-10-06 01:37:54 +0300

    finally got smth

--- main/static/practice.js
+++ main/static/practice.js
@@ -1,24 +1,22 @@
-let Words = document.getElementById("Words");
-const Text = "С другой стороны постоянный количественный рост и сфера нашей активности представляет собой интересный эксперимент проверки направлений прогрессивного развития.";
+const Text = 'с другой стороны постоянный количественный рост и сфера нашей активности';
+const Words = Text.replace(/\S+/g, "<span>$&</span>")
+;
 
 // Variables
 let startTime;
 let inputbox = document.getElementById("InputBox");
 
-Words.textContent = Text
+document.getElementById("Words").innerHTML = Words
+console.log(document.getElementById("Words").innerHTML);
 
 document.getElementById("InputBox").addEventListener('input', function(event) {
-  console.log(document.getElementById("Words"))
   if(Text.slice(0, String(inputbox.value).length) == inputbox.value){
-    console.log("Праильна")
-    console.log("текст - " + Text.slice(0, String(inputbox.value).length))
-    console.log("ввод - " + inputbox.value)
+    console.log("Праильна");
   }
 
   else{
-    console.log("Непраильна")
-    console.log("текст - " + Text.slice(0, String(inputbox.value).length))
-    console.log("ввод - " + inputbox.value)
+    console.log("Непраильна");
   }
-  
+  console.log("текст - " + Text.slice(0, String(inputbox.value).length))
+  console.log("ввод - " + inputbox.value)
 });
