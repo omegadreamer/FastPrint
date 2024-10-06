49ac07f2d364fd27be1e040782287e8aa36f62e8
omegadreamer <lolromik04@gmail.com>
2024-10-06 03:45:59 +0300

    Added wrong/correct check

--- main/static/practice.js
+++ main/static/practice.js
@@ -1,22 +1,31 @@
 const Text = 'с другой стороны постоянный количественный рост и сфера нашей активности';
-const Words = Text.replace(/\S+/g, "<span>$&</span>")
-;
 
 // Variables
 let startTime;
 let inputbox = document.getElementById("InputBox");
 
-document.getElementById("Words").innerHTML = Words
-console.log(document.getElementById("Words").innerHTML);
-
 document.getElementById("InputBox").addEventListener('input', function(event) {
-  if(Text.slice(0, String(inputbox.value).length) == inputbox.value){
+  let input_length = String(inputbox.value).length
+  if(Text.slice(0, input_length) == inputbox.value){
     console.log("Праильна");
+    Words = String('<Correct>' + Text.slice(0, input_length) + '</Correct>' + Text.slice(input_length, Text.length))
   }
 
   else{
+    let wrong = String(inputbox.value)
+    br = 0
+    for (let i = 0; inputbox.value[i] == Text[i]; i++) {
+      wrong = wrong.replace(inputbox.value[i], "")
+    }
+    
+    console.log(wrong.length)
+    console.log(input_length)
+    Words = String(Text.slice(0, input_length - wrong.length) + '<False>' + wrong + '</False>' + Text.slice(input_length - wrong.length, Text.length))
     console.log("Непраильна");
   }
-  console.log("текст - " + Text.slice(0, String(inputbox.value).length))
+
+  document.getElementById("Words").innerHTML = Words;
+  console.log(Words);
+  console.log("текст - " + Text.slice(0, input_length))
   console.log("ввод - " + inputbox.value)
 });
