e3cb8bcf148fee8b41f3148ea50eeff08160e005
omegadreamer <lolromik04@gmail.com>
2024-10-06 10:48:30 +0300

    show wrong half + correct half

--- main/static/practice.js
+++ main/static/practice.js
@@ -4,28 +4,25 @@ const Text = 'с другой стороны постоянный количес
 let startTime;
 let inputbox = document.getElementById("InputBox");
 
-document.getElementById("InputBox").addEventListener('input', function(event) {
-  let input_length = String(inputbox.value).length
-  if(Text.slice(0, input_length) == inputbox.value){
-    console.log("Праильна");
-    Words = String('<Correct>' + Text.slice(0, input_length) + '</Correct>' + Text.slice(input_length, Text.length))
-  }
 
-  else{
-    let wrong = String(inputbox.value)
-    br = 0
-    for (let i = 0; inputbox.value[i] == Text[i]; i++) {
-      wrong = wrong.replace(inputbox.value[i], "")
+document.getElementById("InputBox").addEventListener('input', function(event) {
+  let input_half = String(inputbox.value)
+
+  if(Text.slice(0, input_half.length) == inputbox.value)
+  {
+    console.log("Праильна");
+    Words = String('<Correct>' + Text.slice(0, input_half.length) + '</Correct>' + Text.slice(input_half.length, Text.length))
+  }
+  else
+  {
+    let wrong_half = String(inputbox.value)
+    for (let i = 0; inputbox.value[i] == Text[i]; i++){
+      wrong_half = wrong_half.replace(inputbox.value[i], "")
     }
-    
-    console.log(wrong.length)
-    console.log(input_length)
-    Words = String(Text.slice(0, input_length - wrong.length) + '<False>' + wrong + '</False>' + Text.slice(input_length - wrong.length, Text.length))
+
     console.log("Непраильна");
+    Words = String('<Correct>' + Text.slice(0, input_half.length - wrong_half.length)+ '</Correct>' + '<False>' + wrong_half + '</False>' + Text.slice(input_half.length - wrong_half.length, Text.length))
   }
 
   document.getElementById("Words").innerHTML = Words;
-  console.log(Words);
-  console.log("текст - " + Text.slice(0, input_length))
-  console.log("ввод - " + inputbox.value)
 });
