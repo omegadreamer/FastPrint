1bc7ffeaf7608175c390d4d5540c61fc32e333fc
omegadreamer <lolromik04@gmail.com>
2024-10-07 22:44:28 +0300

    Some CSS

--- main/static/practice.js
+++ main/static/practice.js
@@ -1,4 +1,4 @@
-const Text = 'с другой стороны постоянный количественный рост и сфера нашей активности';
+const Text = 'liberal hit cry root default threshold legend earthwax repetition radiation qualified leash lend rib authority union blonde sister plagiarize resource collar fist arrest start miracle';
 
 // Variables
 let startTime;
@@ -8,13 +8,11 @@ let inputbox = document.getElementById("InputBox");
 document.getElementById("InputBox").addEventListener('input', function(event) {
   let input_half = String(inputbox.value)
 
-  if(Text.slice(0, input_half.length) == inputbox.value)
-  {
+  if(Text.slice(0, input_half.length) == inputbox.value){
     console.log("Праильна");
-    Words = String('<Correct>' + Text.slice(0, input_half.length) + '</Correct>' + Text.slice(input_half.length, Text.length))
-  }
-  else
-  {
+    Words = String('<Correct>' + Text.slice(0, input_half.length) + '</Correct>' + Text.slice(input_half.length, Text.length))}
+
+  else{
     let wrong_half = String(inputbox.value)
     for (let i = 0; inputbox.value[i] == Text[i]; i++){
       wrong_half = wrong_half.replace(inputbox.value[i], "")
