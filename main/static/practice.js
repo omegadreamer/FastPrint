58d1d517159ed954fc6cb30066bb9a36c4bfe5fe
omegadreamer <lolromik04@gmail.com>
2024-10-25 01:10:05 +0300

    Added WPM & CPM results

--- main/static/practice.js
+++ main/static/practice.js
@@ -1,20 +1,28 @@
-import { WordListRus, WordListEng } from "./WordList.js" //list of all words
+import { WordListRus, WordListEng } from "./WordList.js"; //list of all words
 
 
 // Const
 const inputbox = document.getElementById("InputBox");
 const restart = document.getElementById("Restart");
-const lenguage = document.getElementById("Lenguage")
-const Words = document.getElementById("Words")
-const Mistakes = document.getElementById("Mistakes")
+const lenguage = document.getElementById("Lenguage");
+const Words = document.getElementById("Words");
+const Mistakes = document.getElementById("Mistakes");
+const CPM = document.getElementById("CPM");
+const WPM = document.getElementById("WPM");
 
 
-// Variables
+
+// Main variables
 let text = "";
 let words_count = 25;
 let current_lenguage = 1;
-let startTime = 0;
+let test_status = false;
+
+// Stats
 let mistakes = [0,0];
+let timer = 0;
+
+// Lenguages from WordList.js
 let lenguages = {
   0:{
     list: WordListRus,
@@ -25,7 +33,15 @@ let lenguages = {
     name: 'Eng'
   }
 };
-console.log(Object.keys(lenguages).length)
+
+
+// Timer
+setInterval(function() {
+  if(test_status){
+    setTimeout(timer += 1)
+  }
+}, 1000);
+
 
 // Random words from 'WordList' function
 function GetRandomWords() {
@@ -35,17 +51,15 @@ function GetRandomWords() {
   for (let i = 0; i < words_count; i++) {
     text += cur_list[Math.floor(Math.random() * cur_list.length)] + " ";
   }
-  Words.innerHTML = text;
-  mistakes = [0,0]
-  Mistakes.innerHTML = "Mistakes: 0"
-};GetRandomWords();
+  return text;
+};
 
 
 // Active words amount
 document.getElementById("WordsCount").addEventListener("click", function(event) {
   if (words_count != event.target.innerHTML){
     words_count = event.target.innerHTML;
-    GetRandomWords();
+    Restart();
   };
 });
 
@@ -58,21 +72,29 @@ lenguage.addEventListener('click', function(event) {
     current_lenguage = 0;
   };
   lenguage.textContent = String(lenguages[String(current_lenguage)]['name'])
-  GetRandomWords();
+  Restart();
 });
 
 
+// Restart
+function Restart() {
+  test_status = false;
+  timer = 0;
+  Words.innerHTML = GetRandomWords();
+  mistakes = [0,0]
+}Restart();
+
 // Restart button
-restart.addEventListener('click', GetRandomWords);
+restart.addEventListener('click', Restart);
 
 
 // Typing process
 inputbox.addEventListener('input', function(event) {
+  test_status = true;
   let input_half = String(inputbox.value);
   let words = "";
-
-  if(text.slice(0, text.length-1) == inputbox.value){
-    GetRandomWords();
+  if(text.length-1 == inputbox.value.length & !(Words.innerHTML.includes('<false>'))){
+    FinishTyping();
   }else if(text.slice(0, input_half.length) == inputbox.value){
     words = String('<Correct>' + text.slice(0, input_half.length) + '</Correct>' + text.slice(input_half.length, text.length));
     Words.innerHTML = words;
@@ -84,15 +106,27 @@ inputbox.addEventListener('input', function(event) {
   }
 });
 
-  // Mistake counter
+
+// Finish of typing
+function FinishTyping() {
+  let correct_words = text.length - mistakes[1];
+
+  //CPM & WPM
+  CPM.innerHTML = 'CPM ' + correct_words / (timer/60);
+  WPM.innerHTML = 'WPM ' + (correct_words / (timer/60)) / 5;
+  Mistakes.innerHTML = "MST " + '<span>' + mistakes[1] + '</span>';
+
+  Restart()
+}
+
+
+// Mistake counter
 inputbox.addEventListener('input', function(event) {
   if(Words.innerHTML.includes('<false>') & mistakes[0] == 0){
     // mistakes [0,1]
-    mistakes[0] = 1
-    mistakes[1] += 1
+    mistakes[0] = 1;
+    mistakes[1] += 1;
+  }else if(!(Words.innerHTML.includes('<false>'))){
+    mistakes[0] = 0;
   }
-  if(!(Words.innerHTML.includes('<false>'))){
-    mistakes[0] = 0
-  }
-  Mistakes.innerHTML = "Mistakes: " + mistakes[1]
 });
